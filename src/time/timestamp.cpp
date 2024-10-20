410f68d0fcd9ff43a4023de84df55b5829ee2a01
zhonghuashu <77599567@qq.com>
2024-10-20 15:47:01 +0800

    Update timestamp

--- src/time/timestamp.cpp
+++ src/time/timestamp.cpp
@@ -20,20 +20,54 @@ using namespace std::chrono;
 int main(int argc, char *argv[])
 {
 
-    // Use time()
+    /*
+    Use time() -- timer.h from C standard lib.
+    - Time resolution up to seconds.
+    */
+    // Get seconds since 1970 year.
     time_t seconds = ::time((time_t*)nullptr);
-    ::printf("time: %d ms elapsed\n", (int)(seconds * 1000));
+    ::printf("time: %ld ms elapsed\n", (long)(seconds * 1000));
+    tm currTime;
+    // xxx_r: thread-safe function.
+    ::gmtime_r(&seconds, &currTime);
+    ::printf("time: UTC:  %04d-%02d-%02d %02d:%02d:%02d.%03ld\n", currTime.tm_year + 1900, currTime.tm_mon + 1, currTime.tm_mday, currTime.tm_hour, currTime.tm_min, currTime.tm_sec, (long) 0);
+    ::localtime_r(&seconds, &currTime);
+    ::printf("time: local: %04d-%02d-%02d %02d:%02d:%02d.%03ld\n", currTime.tm_year + 1900, currTime.tm_mon + 1, currTime.tm_mday, currTime.tm_hour, currTime.tm_min, currTime.tm_sec, (long) 0);
 
-    // Use gettimeofday()
+    ::printf("\n");
+
+    /*
+    Use gettimeofday() -- sys/time.h from Linux function.
+    - Time resolution up to microseconds (us).
+    */
 	struct timeval tv;
     ::gettimeofday(&tv, nullptr);
-    ::printf("timeval: %d sec, %d us\n", (int) tv.tv_sec, (int) tv.tv_usec);
-    ::printf("timeval: %d ms elapsed\n", (int)((tv.tv_sec * 1000 * 1000 + tv.tv_usec) / 1000));
+    ::printf("gettimeofday: timeval: %d sec, %d us\n", (int) tv.tv_sec, (int) tv.tv_usec);
+    ::printf("gettimeofday: timeval: %d ms elapsed\n", (int)((tv.tv_sec * 1000 * 1000 + tv.tv_usec) / 1000));
+    ::localtime_r(&tv.tv_sec, &currTime);
+    ::printf("gettimeofday: local: %04d-%02d-%02d %02d:%02d:%02d.%03ld\n", currTime.tm_year + 1900, currTime.tm_mon + 1, currTime.tm_mday, currTime.tm_hour, currTime.tm_min, currTime.tm_sec, tv.tv_usec / 1000);
 
-    // Use clock_gettime
+    ::printf("\n");
+
+    /*
+    Use clock_gettime -- timer.h from C standard lib.
+    - Posix High resolution time up to nanoseconds (ns)
+    - CLOCK_REALTIME: Changed as Linux system time
+    - CLOCK_MONOTONIC: Not changed as Linux system time
+    */
     timespec tpstart;
     timespec tpend;
     long timediff;
+
+    // clock_gettime: local: 2024-10-20 14:59:53.942
+    ::clock_gettime(CLOCK_REALTIME, &tpstart);
+    ::localtime_r(&tpstart.tv_sec, &currTime);
+    ::printf("clock_gettime: local: %04d-%02d-%02d %02d:%02d:%02d.%03ld\n", currTime.tm_year + 1900, currTime.tm_mon + 1, currTime.tm_mday, currTime.tm_hour, currTime.tm_min, currTime.tm_sec, tpstart.tv_nsec / 1000 / 1000);
+
+    // clock_gettime: local: 1970-01-01 10:59:13.523
+    ::clock_gettime(CLOCK_MONOTONIC, &tpstart);
+    ::localtime_r(&tpstart.tv_sec, &currTime);
+    ::printf("clock_gettime: local: %04d-%02d-%02d %02d:%02d:%02d.%03ld\n", currTime.tm_year + 1900, currTime.tm_mon + 1, currTime.tm_mday, currTime.tm_hour, currTime.tm_min, currTime.tm_sec, tpstart.tv_nsec / 1000 / 1000);
 
     ::clock_gettime(CLOCK_MONOTONIC, &tpstart);
     ::sleep(1);
@@ -41,30 +75,53 @@ int main(int argc, char *argv[])
     timediff = 1000 * 1000 * (tpend.tv_sec - tpstart.tv_sec) + (tpend.tv_nsec-tpstart.tv_nsec) / 1000;
     ::printf("clock_gettime: %ld ms elapsed\n", timediff / 1000);
 
-    // Use C++11 chrono lib (see https://zhuanlan.zhihu.com/p/559873639)
-    std::cout << "system_clock::is_steady: " << std::boolalpha << system_clock::is_steady << std::endl;
-    std::cout << "steady_clock::is_steady: " << std::boolalpha << steady_clock::is_steady << std::endl;
-    std::cout << "high_resolution_clock::is_steady: " << std::boolalpha << high_resolution_clock::is_steady << std::endl;
+    // Use clock() / time.h function to calculate program running ticks.
+    clock_t startClock;
+    clock_t endClock;
+    startClock = clock();
+    for (int i = 0; i < 100000000; i++) { /* empty */ }
+    endClock = clock();
+    ::printf("clock: %f ms elapsed\n", (double)(endClock - startClock) / CLOCKS_PER_SEC);
+
+    ::printf("\n");
+
+    /*
+    Use C++11 chrono lib (see https://zhuanlan.zhihu.com/p/559873639)
+    - system_clock: up to microseconds (us), Changed according Linux time (e.g., NTP)
+    - steady_clock: up to nanoseconds (ns), suitable for measure program spend time.
+    - high_resolution_clock: up to nanoseconds, equals to high resolution steady_clock.
+    */
+
+    std::cout << "chrono: system_clock::is_steady: " << std::boolalpha << system_clock::is_steady << std::endl;
+    std::cout << "chrono: steady_clock::is_steady: " << std::boolalpha << steady_clock::is_steady << std::endl;
+    std::cout << "chrono: high_resolution_clock::is_steady: " << std::boolalpha << high_resolution_clock::is_steady << std::endl;
 
     system_clock::time_point start = system_clock::now();
-
+    std::time_t timeStart = system_clock::to_time_t(start);
+    ::localtime_r(&timeStart, &currTime);
+    ::printf("chrono: start: %04d-%02d-%02d %02d:%02d:%02d.%03ld\n", currTime.tm_year + 1900, currTime.tm_mon + 1, currTime.tm_mday, currTime.tm_hour, currTime.tm_min, currTime.tm_sec,
+            (::duration_cast<milliseconds>(start.time_since_epoch())).count() % 1000);
     ::sleep(1);
 
-    duration<double> timeGap = system_clock::now() - start;
-    std::cout << "time diff 1: " << ::duration_cast<milliseconds>(timeGap).count() << " ms\n";
+    system_clock::time_point end = system_clock::now();
+    duration<double> timeGap = end - start;
+    std::cout << "chrono: time diff 1: " << ::duration_cast<milliseconds>(timeGap).count() << " ms\n";
 
-    std::time_t timeT = system_clock::to_time_t(system_clock::now());
-    std::cout << "gm time: " << ::asctime(::gmtime(&timeT));
-    std::cout << "local time: " << ::asctime(::localtime(&timeT));
-    tm* currTime = ::localtime(&timeT);
-    ::printf("Current time: %04d-%02d-%02d %02d:%02d:%02d\n", currTime->tm_year + 1900, currTime->tm_mon + 1, currTime->tm_mday,
-            currTime->tm_hour, currTime->tm_min, currTime->tm_sec);
+    std::time_t timeEnd = system_clock::to_time_t(end);
+    std::cout << "chrono: gm time: " << ::asctime(::gmtime(&timeEnd));
+    std::cout << "chrono: local time: " << ::asctime(::localtime(&timeEnd));
+    ::localtime_r(&timeEnd, &currTime);
+    ::printf("chrono: end: %04d-%02d-%02d %02d:%02d:%02d.%03ld\n", currTime.tm_year + 1900, currTime.tm_mon + 1, currTime.tm_mday, currTime.tm_hour, currTime.tm_min, currTime.tm_sec,
+            (::duration_cast<milliseconds>(end.time_since_epoch())).count() % 1000);
 
-    duration<double> timeGap1 = system_clock::now() - system_clock::from_time_t(timeT);
-    std::cout << "time diff 2: " << ::duration_cast<milliseconds>(timeGap1).count() << " ms\n";
+    duration<double> timeGap2 = system_clock::now() - system_clock::from_time_t(timeEnd);
+    std::cout << "chrono: time diff 2: " << ::duration_cast<milliseconds>(timeGap2).count() << " ms\n";
 
+    ::printf("\n");
 
-    // Use timed sem_wait sec
+    /*
+    Use timed sem_wait sec
+    */
     struct timespec ts;
     sem_t sem;
 
@@ -94,6 +151,7 @@ int main(int argc, char *argv[])
     ::printf("Semaphore wait... timeout\n");
     ::sem_destroy(&sem2);
 
+    ::printf("\n");
 
 	return 0;
 }
