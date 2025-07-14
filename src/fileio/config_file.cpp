b0182f1974d07b7f2bda88548682b8dfbacd5b7b
zhonghuashu <77599567@qq.com>
2025-07-14 08:42:52 +0800

    Mac address string to int

--- src/fileio/config_file.cpp
+++ src/fileio/config_file.cpp
@@ -15,8 +15,13 @@ std::string& trim(std::string &s)
         return s;
     }
 
+    // Trim empty space.
     s.erase(0, s.find_first_not_of(" "));
     s.erase(s.find_last_not_of(" ") + 1);
+
+    // Trim double quotation “”
+    s.erase(0, s.find_first_not_of("\""));
+    s.erase(s.find_last_not_of("\"") + 1);
 
     return s;
 }
@@ -52,9 +57,23 @@ void readProperties()
 
 int main()
 {
+    int MAX_LEN = 6;
+    unsigned int mac[MAX_LEN];
     readProperties();
     std::cout << "key1: " << mProperties["key1"] << "\n";
     std::cout << "key2: " << mProperties["key2"] << "\n";
     std::cout << "key3: " << mProperties["key3"] << "\n";
+    std::cout << "key4: " << mProperties["key4"] << "\n";
+
+    // Convert MAC address from string to int array.
+    std::istringstream iss(mProperties["key4"]);
+    for (int i = 0; i < MAX_LEN; i++)
+    {
+        std::string valStr;
+        getline(iss, valStr, ':');
+        mac[i] = (unsigned int) std::stoi(valStr, nullptr, 16);
+    }
+    std::cout << "key4: " << mac[0] << ":" << mac[1] << ":" << mac[2] << ":"
+                          << mac[3] << ":" << mac[4] << ":" << mac[5] << "\n";
     ::exit(0);
 }
